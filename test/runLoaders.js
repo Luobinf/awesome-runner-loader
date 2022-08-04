0f5a215280afd576bc5317c3e03122384c830e0e
Luobinf <1096422441@qq.com>
2022-08-04 10:15:57 +0800

    lib init

--- test/runLoaders.js
+++ test/runLoaders.js
@@ -1,4 +1,21 @@
 require("should");
 const path = require("path");
 const fs = require("fs");
+const runLoaders = require("../lib/LoaderRunner").runLoaders;
 
+describe("test runLoaders", () => {
+    it("should process only a resource",  (done) => {
+		runLoaders({
+			resource: path.resolve(fixtures, "resource.bin")
+		}, function(err, result) {
+			if(err) return done(err);
+			result.result.should.be.eql([Buffer.from("resource", "utf-8")]);
+			result.cacheable.should.be.eql(true);
+			result.fileDependencies.should.be.eql([
+				path.resolve(fixtures, "resource.bin")
+			]);
+			result.contextDependencies.should.be.eql([]);
+			done();
+		});
+    })
+})
